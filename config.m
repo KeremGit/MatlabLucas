b4723320ed4fc1214b732c1dc19347e0177cec8b
kerem <keremalpbilge@gmail.com>
2025-03-31 10:43:36 +0100

    refactor eeg_processing file

--- config.m
+++ config.m
@@ -1,5 +1,6 @@
 function config = config()
 config.basePath = './files/ADHD/';
+config.basePathControl = './files/Control/';
 config.capPath = './files/Standard-10-20-Cap19new/Standard-10-20-Cap19new.ced';
 config.savePath = './files/Preprocessing Data Sets 2/';
 end
